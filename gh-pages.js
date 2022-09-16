6d9ae008124a44a5dc289a88a6317a3b97fc1617
Billy Sillano <billysillano@gmail.com>
2022-09-16 13:56:10 +0800

    make this branch as source

--- gh-pages.js
+++ gh-pages.js
@@ -3,7 +3,7 @@ import ghpages from 'gh-pages';
 ghpages.publish(
     'dist', // path to public directory
     {
-        branch: 'site',
+        branch: 'main',
         repo: 'https://github.com/bo0mburst/splinterguilds.git', // Update to point to your repository  
         user: {
             name: 'Billy Sillano', // update to use your name
